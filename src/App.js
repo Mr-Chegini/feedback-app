19558fb6dc1e4832f8caecc57dae66623ad21519
Amir Chegini <amirchegini007@gmail.com>
2022-02-05 15:13:09 +0330

    Create first componnet

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-function App() {
-  const title = `blog post`;
-  const body = `This is my blog post`;
-  const comments = [
-    { id: 1, text: `Comment one` },
-    { id: 2, text: `Comment two` },
-    { id: 3, text: `Comment three` },
-  ];
-  const showComments = true;
-  const commentsBlock = (
-    <div className="comments">
-      <h3>Comments ({comments.length})</h3>
-      <ul>
-        {comments.map((comment, index) => (
-          <li key={index}>{comment.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
+import Header from "./components/Header";
 
+function App() {
   return (
-    <div className="container">
-      <h1>{title.toUpperCase()}</h1>
-      <p>{body}</p>
-      {showComments && commentsBlock}
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
   );
 }
 
