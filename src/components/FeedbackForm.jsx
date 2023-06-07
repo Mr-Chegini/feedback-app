089f5f81445dce2477488802d7c8fc63d2069770
Amir Chegini <amirchegini007@gmail.com>
2023-06-08 03:01:38 +0430

    Implement FeedbackForm component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,9 +1,27 @@
-import  Card from "./shared/Card";
+import { useState } from "react";
+
+import Card from "./shared/Card";
 
 function FeedbackForm() {
-      
-    return <Card>
-  </Card>;
+  const [text, setText] = useState("");
+  const handleText = (e) => setText(e.target.value);
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        <div className="input-group">
+          <input
+            onChange={handleText}
+            type="text"
+            className="form-control"
+            placeholder="Write a review"
+            value={text}
+          ></input>
+          <button type="submit">Send</button>
+        </div>
+      </form>
+    </Card>
+  );
 }
 
 export default FeedbackForm;
